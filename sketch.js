684d9eb7255d72c8a6383ca775082eb8b2bbf6fd
noahbei <nbeidelman9@gmail.com>
2024-01-24 16:20:18 -0800

    add ability to change sideLength plus input fields

--- sketch.js
+++ sketch.js
@@ -1,9 +1,17 @@
+let sizeInput = document.getElementById("cubeSize");
+let spaceInput = document.getElementById("spacebetween");
+
+
+
 let i = 0;
 let a = true;
 let randDirection = true;
 
-const sideLength = 16;
-const planeNum = 3 * sideLength;
+let sideLength = 2;
+let planeNum = 3 * sideLength;
+let cubeSize = 30;
+let spaceBetween = 10;
+let totalArea = cubeSize + spaceBetween;
 
 function setup() {
   createCanvas(710, 400, WEBGL);
@@ -47,7 +55,7 @@ function drawBoxes() {
 function drawCube(x, y, z) {
   push();
   translate(x, y, z);
-  box(30, 30);
+  box(cubeSize, cubeSize);
   pop();
 }
 
@@ -62,9 +70,9 @@ function generateAxisFunctions(axis, depth) {
         functionBody += `${axis}Order${j}();\n`;
       }
     }
-    functionBody += `translate(sideLength/2 * 40 - 20, sideLength/2 * 40- 20, sideLength/2 * 40- 20);`;
+    functionBody += `translate(sideLength/2 * totalArea - totalArea/2, sideLength/2 * totalArea- totalArea/2, sideLength/2 * totalArea- totalArea/2);`;
     functionBody += `rotate${axis.toUpperCase()}(direction ? i : -i);\n`;
-    functionBody += `translate(-sideLength/2 * 40 + 20, -sideLength/2 * 40+ 20, -sideLength/2 * 40+ 20);`;
+    functionBody += `translate(-sideLength/2 * totalArea + totalArea/2, -sideLength/2 * totalArea+ totalArea/2, -sideLength/2 * totalArea+ totalArea/2);`;
     functionBody += `${axis}Order${i}();\n`;
 
     functions.push(
@@ -77,9 +85,9 @@ function generateAxisFunctions(axis, depth) {
   return functions;
 }
 
-let xOrderFunctions = generateXFunctions(sideLength);
-let yOrderFunctions = generateYFunctions(sideLength);
-let zOrderFunctions = generateZFunctions(sideLength);
+generateXFunctions(sideLength);
+generateYFunctions(sideLength);
+generateZFunctions(sideLength);
 
 let xFunctions = generateAxisFunctions("x", sideLength);
 let yFunctions = generateAxisFunctions("y", sideLength);
@@ -94,7 +102,7 @@ function generateZFunctions(depth) {
 
     for (let x = 0; x < depth; x++) {
       for (let y = 0; y < depth; y++) {
-        functionBody += `drawCube(${x * 40}, ${y * 40}, ${i * 40});\n`;
+        functionBody += `drawCube(${x * totalArea}, ${y * totalArea}, ${i * totalArea});\n`;
       }
     }
 
@@ -111,7 +119,7 @@ function generateYFunctions(depth) {
 
     for (let x = 0; x < depth; x++) {
       for (let z = 0; z < depth; z++) {
-        functionBody += `drawCube(${x * 40}, ${i * 40}, ${z * 40});\n`;
+        functionBody += `drawCube(${x * totalArea}, ${i * totalArea}, ${z * totalArea});\n`;
       }
     }
 
@@ -128,7 +136,7 @@ function generateXFunctions(depth) {
 
     for (let y = 0; y < depth; y++) {
       for (let z = 0; z < depth; z++) {
-        functionBody += `drawCube(${i * 40}, ${y * 40}, ${z * 40});\n`;
+        functionBody += `drawCube(${i * totalArea}, ${y * totalArea}, ${z * totalArea});\n`;
       }
     }
 
@@ -137,3 +145,6 @@ function generateXFunctions(depth) {
     `);
   }
 }
+sizeInput.addEventListener("input", e => {
+  cubeSize = sizeInput.textContent
+})
